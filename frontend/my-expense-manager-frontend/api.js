6fa1653a841afa5ce1cb15466626888f37047ec7
shiv-das <87539999+shiv-das@users.noreply.github.com>
2023-03-26 23:44:49 +0530

    fixed axios error

--- frontend/my-expense-manager-frontend/api.js
+++ frontend/my-expense-manager-frontend/api.js
@@ -5,14 +5,12 @@ if (typeof window !== "undefined") {
   authKey = localStorage.getItem("x-auth-token");
 }
 
-export default axios.create(() => {
-  return {
-    baseURL: "http://localhost:8080",
-    headers: {
-      authorization: authKey,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  };
+export default axios.create({
+  baseURL: "http://localhost:8080",
+  headers: {
+    authorization: authKey,
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });

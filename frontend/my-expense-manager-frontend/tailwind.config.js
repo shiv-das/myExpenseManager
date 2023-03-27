ba98b4300ea1c2a48f736e7d96c4f8d836779152
shiv-das <87539999+shiv-das@users.noreply.github.com>
2023-03-27 10:25:44 +0530

    added Registration Modal

--- frontend/my-expense-manager-frontend/tailwind.config.js
+++ frontend/my-expense-manager-frontend/tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };

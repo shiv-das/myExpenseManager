835123d9e6d6d97806b0d4a0690796dba1f7faf4
shiv-das <87539999+shiv-das@users.noreply.github.com>
2023-03-27 11:21:23 +0530

    fixed input type Date

--- frontend/my-expense-manager-frontend/components/RegistrationModal.js
+++ frontend/my-expense-manager-frontend/components/RegistrationModal.js
@@ -105,9 +105,7 @@ export default function RegistrationModal(props) {
                   Date of Expense
                 </label>
                 <input
-                  datepicker="true"
-                  datepicker-autohide="true"
-                  type="text"
+                  type="date"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Date of Expense"
                 />
